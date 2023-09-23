8873594312136ae9817b27f5557aa63c00f8dea4
Дід Alik <didalik.aim@gmail.com>
2023-09-23 20:30:17 +0000

    Fix dak/util/meta

--- dak/svc/registry/genesis/bin/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-#npx esbuild --bundle --sourcemap --loader:.html=text --minify \
-#  --loader:.ico=binary --outfile=dist/index.mjs --format=esm ./src/index.mjs
-
-## no --sourcemap
-#
-npx esbuild --bundle --loader:.html=text --minify \
-  --loader:.ico=binary --outfile=dist/index.mjs --format=esm ./src/index.mjs
